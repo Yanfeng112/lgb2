4210ea9b59f75cc5ae60115439de0a312c596bbe
gelande2211 <88170328+Yanfeng112@users.noreply.github.com>
2025-05-28 20:12:02 +0800

    Update 程序1APP.py

--- 程序1APP.py
+++ 程序1APP.py
@@ -54,7 +54,7 @@ if st.button("Predict"):
     probability = predicted_proba[predicted_class] * 100
 
     # 显示预测结果，使用 Matplotlib 渲染指定字体
-    text = f"Based on feature values, predicted possibility of AKI is {probability:.2f}%"
+    text = f"基于以上环境特征，该地区的发生黑热病的风险概率为 {probability:.2f}%"
     fig, ax = plt.subplots(figsize=(8, 1))
     ax.text(
         0.5, 0.5, text,
